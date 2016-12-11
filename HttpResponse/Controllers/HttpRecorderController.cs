e812df8f0b18c43a2c905c3203fdfdf04f681b4f
laza <laszlo.zahoranszky@gmail.com>
2016-12-11 01:42:41 +0100

    Initial commit

---
+++ HttpResponse/Controllers/HttpRecorderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HttpResponse.Controllers
+{
+    public class HttpRecorderController : ApiController
+    {
+        // GET: api/HttpRecorder
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "This is HttpRecorderController!" };
+        }
+    }
+}
