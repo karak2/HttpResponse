bf187d71866b86e4991dc0d84ede67714ee12a45
laza <laszlo.zahoranszky@gmail.com>
2016-12-11 02:46:02 +0100

    print ip of cilent

--- HttpResponse/Controllers/HttpRecorderController.cs
+++ HttpResponse/Controllers/HttpRecorderController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.ServiceModel.Channels;
+using System.Web;
 using System.Web.Http;
 
 namespace HttpResponse.Controllers
@@ -10,9 +9,32 @@ namespace HttpResponse.Controllers
     public class HttpRecorderController : ApiController
     {
         // GET: api/HttpRecorder
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "This is HttpRecorderController!" };
+            return GetClientIp();
+        }
+
+        private string GetClientIp(HttpRequestMessage request = null)
+        {
+            request = request ?? Request;
+
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }
+            else if (request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty prop = (RemoteEndpointMessageProperty)request.Properties[RemoteEndpointMessageProperty.Name];
+                return prop.Address;
+            }
+            else if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+            else
+            {
+                return "No client IP found";
+            }
         }
     }
 }
